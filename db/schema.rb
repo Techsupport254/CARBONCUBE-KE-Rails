5c68bd85990a9d1f0f612a636ed35ad4566bceb8
Techsupport254 <victorquaint@gmail.com>
2026-01-08 10:25:44 +0300

    Add 2026 premium tier migration and update authentication logic
    - Update authentication controller to give premium tiers to all users in 2026
    - Add migration to give all existing sellers premium tier for 2026
    - Update schema with new migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_30_164148) do
+ActiveRecord::Schema[7.1].define(version: 2026_01_08_072126) do
   create_schema "auth"
   create_schema "extensions"
   create_schema "graphql"
