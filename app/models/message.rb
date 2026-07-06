316e860465f7bddb51cdb382b96d76db89316c03
Victor Kirui <victorquaint@gmail.com>
2026-07-06 15:21:43 +0300

    Fix WhatsApp message callback - remove update_sender_last_active

--- app/models/message.rb
+++ app/models/message.rb
@@ -7,7 +7,6 @@ class Message < ApplicationRecord
 
   # Callbacks
   after_create :update_conversation_timestamp
-  after_create :update_sender_last_active
   after_create :broadcast_new_message
   after_create :schedule_delivery_receipt
   after_create :send_push_notification
@@ -72,16 +71,6 @@ class Message < ApplicationRecord
   def update_conversation_timestamp
     # Update the conversation's updated_at to reflect the new message
     conversation.touch
-  end
-
-  def update_sender_last_active
-    # Update sender's last_active_at when they send a message
-    if sender.respond_to?(:update_last_active!)
-      sender.update_last_active!
-    end
-  rescue => e
-    Rails.logger.warn "Failed to update sender last_active_at: #{e.message}"
-    # Don't fail message creation if this fails
   end
 
   def schedule_delivery_receipt
